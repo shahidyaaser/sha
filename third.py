26a8805f369e1380000cdebd7713c3e1ceb33c75
shahidyaaser <42862587+shahidyaaser@users.noreply.github.com>
2018-08-31 15:24:43 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,13 @@
+# Python Program - Check Vowel or Not
+
+print("Enter '0' for exit.");
+ch = input("Enter any character: ");
+if ch == '0':
+    exit();
+else:
+    if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I'
+       or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+    	    print(ch, "is a vowel.");
+    else:
+    	print(ch, "is not a vowel.");
+      
